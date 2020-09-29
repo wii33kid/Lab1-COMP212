9590c7827b232dd54e4eea3db2ff2f33b755aef2
Michael Magtibay <mj.magtibay@hotmail.com>
2020-09-28 23:48:24 -0400

    added delegate to subscriber
    changed <subscriber> to string
    
    coded sendviamobile class

--- Lab1-COMP212/Classes/Subscriber.cs
+++ Lab1-COMP212/Classes/Subscriber.cs
@@ -9,9 +9,66 @@ namespace Lab1_COMP212
     public class Subscriber
     {
 
-        public String Email {get; set;}
+        public String Email { get; set; }
         public long phoneNumber { get; set; }
 
-        public static List<Subscriber> SubscriberList = new List<Subscriber>();
+        public static List<String> SubscriberList = new List<string>();
+        private int returnValue = 0;
+        private string returnMessage = string.Empty;
+
+
+        public delegate string SubscribeDel(string _value, string _value2);
+
+        public SubscribeDel GetSubscribeTypeDel(string subscribeType)
+        {
+            SubscribeDel subscribeDel = null;
+            switch (subscribeType.ToLower())
+            {
+                case "email":
+
+                    break;
+                case "phone":
+                    subscribeDel = SubscribeMobile;
+                    break;
+                default: break;
+            }
+            return subscribeDel;
+        }
+
+        //Subscribe mobile
+        private string SubscribeMobile(string value, string value2)
+        {
+            // ** Call mobile class
+            Classes.SendViaMobile sendViaMobile = new Classes.SendViaMobile(value);
+
+            switch (value2.ToLower())
+            {
+                case "subscribe":
+                    this.returnValue = sendViaMobile.Subscribe(SubscriberList);
+                    if (this.returnValue == 1)
+                    {
+                        this.returnMessage = $"ERROR: {value} has already been created.";
+                    }
+                    else
+                    {
+                        this.returnMessage = $"Successfully {value2} {value}.";
+                    }
+                    break;
+                case "unsubscribe":
+                    this.returnValue = sendViaMobile.Unsubscribe(SubscriberList);
+                    if (this.returnValue < 0)
+                    {
+                        this.returnMessage = $"ERROR: {value} does not exist.";
+                    }
+                    else
+                    {
+                        this.returnMessage = $"Successfully {value2} {value}.";
+                    }
+                    break;
+                default: break;
+            }
+
+            return this.returnMessage;
+        }
     }
 }
